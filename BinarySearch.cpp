84bbbfb3fe6c592d0fd0bb1f584dbb712c988aca
Aseel <asilalhashmi@email.com>
2026-04-21 14:09:48 +0700

    Add input validation for array length in BinarySearch.cpp
    Introduced an input() function to prompt the user for the number of elements, ensuring the value is between 1 and 10. Updated main() to call input() before exiting, improving input validation and user guidance.

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -6,11 +6,24 @@ using namespace std;
 int element[10];
 int length;
 int x;
+void input() 
+{
 
+	while (true)
+	{
+		
+		cout << "number of elements must be between 1 and 10\n\n";
+
+		cout << "Enter the number of elements ? "; cin >> length;
+		if (length > 0 && length < 11)
+			break;
+
+	}
+}
 int main()
 {
 
-
+	input();
 	
 	return 0;
 }
